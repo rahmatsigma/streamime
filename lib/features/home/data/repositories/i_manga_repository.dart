47a6813ddfa09d08037c9a264d41dc8c201cf136
Rendyprobe <rendy221205@gmail.com>
2025-11-11 13:49:53 +0700

    Deploy Flutter Web to branch 'deploy'

--- lib/features/home/data/repositories/i_manga_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-typedef Failure = dynamic;
-typedef MangaList = List<dynamic>;
-
-abstract class IMangaRepository {
-  Future<Either<Failure, MangaList>> getPopularManga({required int page});
-  Future<Either<Failure, MangaList>> searchManga({required String query});
-}
