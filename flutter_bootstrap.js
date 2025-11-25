093615687261d248a0bbb353a796cdfd81f7f03c
rahmatsigma <rahmatsigma@users.noreply.github.com>
2025-11-25 13:44:02 +0000

    deploy: 4b4d0617d70973d081e47f072f5f0b0f370fc600

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2141593771"
+    serviceWorkerVersion: "3070413139"
   }
 });
