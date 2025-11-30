6e2009854876723a5411f6e910e7104b7dae5c24
VhemasP <VhemasP@users.noreply.github.com>
2025-11-30 16:35:02 +0000

    deploy: 9dde3e494ba7fcda407fe09acf6be62ae5b5985c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1208604035"
+    serviceWorkerVersion: "1103627073"
   }
 });
